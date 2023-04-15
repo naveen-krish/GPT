52eac7914a971b1e36727c15e555d186dda01070
Naveen <naveenkrishiv@gmail.com>
2023-04-15 18:20:35 +0530

    azure functions with Gpt-3

---
+++ OpenAICompletion/__init__.py
@@ -0,0 +1,25 @@
+import logging
+import openai
+import azure.functions as func
+
+# sample request body
+# {"model":"text-davinci-003","prompt":"This is a test","max_tokens":200,"temperature":0}
+
+secret_key = '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'
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    openai.api_key = secret_key
+    req_body = req.get_json()
+
+    output = openai.Completion.create(
+        model=req_body['model'],
+        prompt=req_body['prompt'],
+        max_tokens=req_body['max_tokens'],
+        temperature=req_body['temperature']
+    )
+
+    output_text = output['choices'][0]['text']
+    return func.HttpResponse(output_text, status_code=200)
